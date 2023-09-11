6c85c372e6beb226d2f93af9c187ac1974384307
Gnom204 <113632977+Gnom204@users.noreply.github.com>
2023-09-11 22:26:57 +0300

    hotfix: обновление ссылки API

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = 'https://api.dipgnom.nomoredomains.xyz';
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://api.dipgnom.nomoredomains.xyz';
 export const BASE_MOVIE_URL = 'https://api.nomoreparties.co/beatfilm-movies'
